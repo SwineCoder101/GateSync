e51cf1c5a18cfbd2fabe6e42edd0961002d2ca0c
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-11-16 11:49:42 +0700

    add-privy-provider-frontend

---
+++ src/providers/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { PrivyProvider, PrivyClientConfig } from "@privy-io/react-auth";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+
+const solanaConnectors = toSolanaWalletConnectors({
+  // By default, shouldAutoConnect is enabled
+  shouldAutoConnect: true,
+});
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      // appId="clra3wyj700lslb0frokrj261"
+      config={{
+        embeddedWallets: {
+          createOnLogin: "all-users",
+        },
+        appearance: { theme: "light", accentColor: "#676FFF", walletChainType: "ethereum-and-solana" },
+        externalWallets: { solana: { connectors: solanaConnectors } },
+      }}
+    >
+      <div>{children}</div>
+    </PrivyProvider>
+  );
+}
